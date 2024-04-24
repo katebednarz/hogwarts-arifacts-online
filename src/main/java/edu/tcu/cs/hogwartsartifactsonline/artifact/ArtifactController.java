aab10a055598b319f0bcfd1fff647968ddb341a2
katebednarz <112124950+katebednarz@users.noreply.github.com>
2024-04-24 18:00:21 -0500

    Implement find artifacts by search criteria with Spring Data JPA Specifications.

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactController.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactController.java
@@ -2,6 +2,7 @@ package edu.tcu.cs.hogwartsartifactsonline.artifact;
 
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -91,5 +92,12 @@ public class ArtifactController {
                 .collect(Collectors.toList());
         String artifactSummary = this.artifactService.summarize(artifactDtos);
         return new Result(true, StatusCode.SUCCESS, "Summarize Success", artifactSummary);
+    }
+
+    @PostMapping("/search")
+    public Result findArtifactsByCriteria(@RequestBody Map<String, String> searchCriteria, Pageable pageable){
+        Page<Artifact> artifactPage = this.artifactService.findByCriteria(searchCriteria, pageable);
+        Page<ArtifactDto> artifactDtoPage = artifactPage.map(this.artifactToArtifactDtoConverter::convert);
+        return new Result(true, StatusCode.SUCCESS, "Search Success", artifactDtoPage);
     }
 }
